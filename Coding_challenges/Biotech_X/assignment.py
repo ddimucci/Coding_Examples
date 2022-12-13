ffb0c9bcf44d6dab1822a832808e0872ca9afbe3
ddimucci <36259203+ddimucci@users.noreply.github.com>
2022-12-13 18:40:44 -0500

    Update assignment.py

--- Coding_challenges/Biotech_X/assignment.py
+++ Coding_challenges/Biotech_X/assignment.py
@@ -1,1 +1,62 @@
+from itertools import product, combinations
+from collections import Counter
 
+translation_table = {'TTT': 'F', 'TCT': 'S', 'TAT': 'Y', 'TGT': 'C',
+                     'TTC': 'F', 'TCC': 'S', 'TAC': 'Y', 'TGC': 'C',
+                     'TTA': 'L', 'TCA': 'S', 'TAA': '*', 'TGA': '*',
+                     'TTG': 'L', 'TCG': 'S', 'TAG': '*', 'TGG': 'W',
+                     'CTT': 'L', 'CCT': 'P', 'CAT': 'H', 'CGT': 'R',
+                     'CTC': 'L', 'CCC': 'P', 'CAC': 'H', 'CGC': 'R',
+                     'CTA': 'L', 'CCA': 'P', 'CAA': 'Q', 'CGA': 'R',
+                     'CTG': 'L', 'CCG': 'P', 'CAG': 'Q', 'CGG': 'R',
+                     'ATT': 'I', 'ACT': 'T', 'AAT': 'N', 'AGT': 'S',
+                     'ATC': 'I', 'ACC': 'T', 'AAC': 'N', 'AGC': 'S',
+                     'ATA': 'I', 'ACA': 'T', 'AAA': 'K', 'AGA': 'R',
+                     'ATG': 'M', 'ACG': 'T', 'AAG': 'K', 'AGG': 'R',
+                     'GTT': 'V', 'GCT': 'A', 'GAT': 'D', 'GGT': 'G',
+                     'GTC': 'V', 'GCC': 'A', 'GAC': 'D', 'GGC': 'G',
+                     'GTA': 'V', 'GCA': 'A', 'GAA': 'E', 'GGA': 'G',
+                     'GTG': 'V', 'GCG': 'A', 'GAG': 'E', 'GGG': 'G'
+                     }
+
+# nomenclature for degenerate codons
+expanded_code = {'A': ['A'], 'C': ['C'], 'G': ['G'], 'T': ['T'],
+                 'W': ['A', 'T'], 'S': ['C', 'G'], 'M': ['A', 'C'], 'K': ['G', 'T'], 'R': ['A', 'G'], 'Y': ['C', 'T'],
+                 'B': ['C', 'G', 'T'], 'D': ['A', 'G', 'T'], 'H': ['A', 'C', 'T'], 'V': ['A', 'C', 'G'],
+                 'N': ['A', 'C', 'G', 'T']
+                 }
+
+# helpful for validating input
+valid_nucleotides = 'ACGTWSMKRYBDHVN'
+valid_aa = 'GAVLIMFWPSTCYNQDEKRH*'
+
+
+def get_codon_for_amino_acids(amino_acids):
+    """
+    :param amino_acids: set
+        the amino acids we want to code for, i.e. {'A','I','V'}
+    :rtype: set, float
+        returns two values the set of most efficient codons for the input set list, e.g. {'RYA', 'RYH', 'RYC', 'RYW', 'RYM', 'RYY', 'RYT'} and the achieved efficiency e.g. 0.75
+    """
+
+    pass
+
+
+def truncate_list_of_amino_acids(amino_acids):
+    """
+    :param amino_acids: set
+        the amino acids we want to code for, i.e. {'A','I','V'}
+    :rtype: set
+        the set of sets of amino acids that can be coded with 100% efficiency, i.e. {frozenset({'V', 'A'}), frozenset({'V', 'I'})}
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    # using sets instead of lists throughout the code since the oder doesn't matter and all items should be unique
+    assert get_codon_for_amino_acids({'A', 'I', 'V'}) == ({'RYA', 'RYH', 'RYC', 'RYW', 'RYM', 'RYY', 'RYT'}, 0.75)
+    assert get_codon_for_amino_acids({'M', 'F'}) == ({'WTS', 'WTK', "WTB"}, 0.5)
+
+    # "frozenset" here since this seems to be the only way to get a set of sets - see https://stackoverflow.com/questions/5931291/how-can-i-create-a-set-of-sets-in-python
+    assert truncate_list_of_amino_acids({'A', 'V', 'I'}) == {frozenset({'V', 'A'}), frozenset({'V', 'I'})}
